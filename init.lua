3982ec2abded5fdc5569428d6b672df49d034b16
Michal Trybus <M.Trybus@adbglobal.com>
2024-05-10 20:14:42 +0200

    a few option changes + readline/emacs insert-mode shortcuts

--- init.lua
+++ init.lua
@@ -30,8 +30,19 @@ vim.opt.timeoutlen = 500
 vim.opt.updatetime = 500
 vim.opt.inccommand = "split"
 vim.opt.scrolloff = 8
+vim.opt.relativenumber = true
 
 vim.keymap.set('n', '<F2>', function() vim.o.cursorcolumn = not vim.o.cursorcolumn end, { expr = true })
 vim.keymap.set('n', '<F3>', ':IBLToggle<cr>')
+
+-- A little bit of emacs can't be too sacrilegious... right?
+--
+-- These are mainly for telescope/dressing, whic use insert mode for user input.
+-- It's kind of hard for my brain to distinguish it from typing in shell, so I need some basic readline shortcuts.
+-- Command line is handled by ryvnf/readline.vim.
+vim.keymap.set('i', '<C-a>', '<C-o>^')
+vim.keymap.set('i', '<C-e>', '<C-o>$')
+vim.keymap.set('i', '<C-b>', '<C-o>h')
+vim.keymap.set('i', '<C-f>', '<C-o>l')
 
 vim.cmd 'source ~/.config/nvim/legacy.vim'
--- lua/plugins/context.lua
+++ lua/plugins/context.lua
@@ -6,9 +6,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
   },
   opts = {
-    max_lines = 5,
     min_window_height = 25,
-    multiline_threshold = 3,
     trim_scope = 'outer',
     mode = 'cursor',
   },
