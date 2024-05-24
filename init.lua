0f9b284bc9bac541ccee20aca4b16a0e623d74b3
Michal Trybus <M.Trybus@adbglobal.com>
2024-05-24 07:24:21 +0200

    \x to remove surrounding parentheses

--- init.lua
+++ init.lua
@@ -45,6 +45,17 @@ vim.keymap.set('i', '<C-e>', '<C-o>$')
 vim.keymap.set('i', '<C-b>', '<C-o>h')
 vim.keymap.set('i', '<C-f>', '<C-o>l')
 
+-- Remove both the character under the cursor and its match
+vim.keymap.set('n', '<leader>x', function()
+  local col = vim.api.nvim_win_get_cursor(0)[2]
+  local char = string.sub(vim.api.nvim_get_current_line(), col+1, col+1)
+  if string.find([[ ([< ]], char, 1, true) then
+    vim.api.nvim_feedkeys("mm%x`mx", 'n', true)
+  elseif string.find([[ )]> ]], char, 1, true) then
+    vim.api.nvim_feedkeys("%mm%x`mx", 'n', true)
+  end
+end)
+
 vim.api.nvim_create_user_command('EditConfig', function()
   local config_dir = vim.fn.stdpath('config')
   assert(type(config_dir) == 'string', 'Expected string')
