6d0f67dcbac222148a49ad2a0d033e587c038814
Michal Trybus <komar007@gmail.com>
2024-06-02 22:29:17 +0200

    [lsp] code actions open near cursor

--- lua/plugins/actions_preview.lua
+++ lua/plugins/actions_preview.lua
@@ -6,6 +6,17 @@ return {
     'neovim/nvim-lspconfig',
   },
   lazy = false,
+  opts = function()
+    return {
+      telescope = require('telescope.themes').get_dropdown({
+        layout_config = {
+          height = 20,
+          width = 125,
+        },
+        layout_strategy = 'cursor',
+      })
+    }
+  end,
   keys = {
     { "ga", (function() require("actions-preview").code_actions() end), mode = { "v", "n" } },
   },
