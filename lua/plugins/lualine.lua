330b4dac2ba3f2dcb56b1918fd4fafd6fdeb9ae5
Michal Trybus <M.Trybus@adbglobal.com>
2024-06-03 15:15:39 +0200

    minimized lsp progress
    also added forgotten entry to lazy-lock.json

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -55,6 +55,15 @@ local function search_result()
   return ' ' .. last_search .. ' (' .. searchcount.current .. '/' .. searchcount.total .. ')'
 end
 
+local function duplicate(inputTable)
+  local outputTable = {}
+  for i = 1, #inputTable do
+    table.insert(outputTable, inputTable[i])
+    table.insert(outputTable, inputTable[i])
+  end
+  return outputTable
+end
+
 return {
   'nvim-lualine/lualine.nvim',
 
@@ -76,6 +85,21 @@ return {
               return '  '
             end,
             padding = 1,
+          },
+          {
+            'lsp_progress',
+            display_components = { 'spinner', {} },
+            separators = {
+              component = '',
+              progress = '',
+              message = { pre = '', post = '' },
+              percentage = { pre = '', post = '' },
+              title = { pre = '', post = '' },
+              lsp_client_name = { pre = '', post = '' },
+              spinner = { pre = '', post = '' },
+            },
+            spinner_symbols = duplicate({ "▱▱▱", "▰▱▱", "▰▰▱", "▰▰▰", "▱▰▰", "▱▱▰" }),
+            padding = { left = 0, right = 2 },
           },
         },
         lualine_b = {
@@ -166,10 +190,6 @@ return {
                 vim.diagnostic.goto_prev();
               end
             end,
-          },
-          {
-            'lsp_progress',
-            display_components = { 'spinner', { 'title', 'percentage' } },
           },
         },
         lualine_x = {
