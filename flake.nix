c4fe56688afd6900049092f00d104c508f7fd809
Michal Trybus <M.Trybus@adbglobal.com>
2024-08-01 17:02:55 +0200

    [lsp] added marksman for markdown

--- flake.nix
+++ flake.nix
@@ -36,6 +36,8 @@
               docker-compose-language-service
               # gopls
               go gopls
+              # marksman
+              marksman
               # jsonls
               nodePackages.vscode-json-languageserver
               # lua_ls
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -58,6 +58,7 @@ return {
         'dockerls',
         'eslint',
         'gopls',
+        'marksman',
         'nixd',
         'vimls',
       },
