f031dd2edfa5385fd209235c4bb9b7467273e517
Michal Trybus <M.Trybus@adbglobal.com>
2024-10-16 13:42:10 +0200

    [lsp] better codelens refresh

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -41,7 +41,8 @@ return {
       end
 
       if client.server_capabilities.codeLensProvider then
-        vim.api.nvim_create_autocmd({ "BufEnter", "InsertLeave" }, {
+        vim.lsp.codelens.refresh()
+        vim.api.nvim_create_autocmd({ "InsertLeave", "BufWritePost" }, {
           callback = vim.lsp.codelens.refresh,
           desc = "Refresh codelens",
           buffer = 0,
