d50312f15c6e18ff6f4b351577388429fa1d38ed
Michal Trybus <komar007@gmail.com>
2024-12-09 21:58:08 +0100

    [lsp] switched lua development to folke/lazydev

---
+++ lua/plugins/lazydev.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {
+      library = {
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, {
+        name = "lazydev",
+        -- set group index to 0 to skip loading LuaLS completions
+        group_index = 0,
+      })
+    end,
+  },
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -83,6 +83,7 @@ return {
           'jsonls',
           capabilities = caps_for_jsonls,
         },
+        'lua_ls',
         'marksman',
         'nixd',
         'protols', -- not available in nixpkgs, must be installed with: cargo install protols
@@ -157,40 +158,6 @@ return {
           min_chars = 3,
         }
       },
-    }
-
-    require('lspconfig').lua_ls.setup {
-      capabilities = capabilities,
-      on_attach = on_lsp_attach,
-      on_init = function(client)
-        local path = client.workspace_folders[1].name
-        if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
-          return
-        end
-
-        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-          runtime = {
-            -- Tell the language server which version of Lua you're using
-            -- (most likely LuaJIT in the case of Neovim)
-            version = 'LuaJIT'
-          },
-          -- Make the server aware of Neovim runtime files
-          workspace = {
-            checkThirdParty = false,
-            library = {
-              vim.env.VIMRUNTIME
-              -- Depending on the usage, you might want to add additional paths here.
-              -- "${3rd}/luv/library"
-              -- "${3rd}/busted/library",
-            }
-            -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-            -- library = vim.api.nvim_get_runtime_file("", true)
-          }
-        })
-      end,
-      settings = {
-        Lua = {}
-      }
     }
 
     vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
