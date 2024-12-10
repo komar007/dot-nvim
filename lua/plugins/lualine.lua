32d490508f1ae69e6407f0d61798d8ef2301de38
Michal Trybus <M.Trybus@adbglobal.com>
2024-12-10 12:48:20 +0100

    [lualine] fixed lsp server list to just include current buffer (broken after deprecation warnings removed)

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -71,7 +71,7 @@ return {
           {
             function()
               local out = ''
-              if next(vim.lsp.get_clients()) == nil then
+              if next(vim.lsp.get_clients({ bufnr = 0 })) == nil then
                 out = ' ' .. out .. '  '
               end
               return out
@@ -82,7 +82,7 @@ return {
             function()
               local out = ''
               local added = false
-              for _, client in pairs(vim.lsp.get_clients()) do
+              for _, client in pairs(vim.lsp.get_clients({ bufnr = 0 })) do
                 if added then
                   out = out .. ','
                 else
