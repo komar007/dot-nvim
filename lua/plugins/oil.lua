9f10c53b334e7819549542de6dde6a67d3958c5c
Michal Trybus <M.Trybus@adbglobal.com>
2024-12-13 12:35:44 +0100

    [oil] git integration

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,7 +2,13 @@ return {
   'stevearc/oil.nvim',
 
   lazy = false,
-  opts = {},
+  opts = {
+    git = {
+      add = function(path) return false end,
+      mv = function(src_path, dest_path) return true end,
+      rm = function(path) return true end,
+    },
+  },
   dependencies = { "nvim-tree/nvim-web-devicons" },
   keys = {
     { "-", "<cmd>Oil<CR>" },
