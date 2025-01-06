dd5266caf925a8171a0d29761b3b739c29ff853e
Michal Trybus <komar007@gmail.com>
2025-01-06 22:18:08 +0100

    [git] changed removed&modified gitgutter icon

--- lua/plugins/gitgutter.lua
+++ lua/plugins/gitgutter.lua
@@ -8,7 +8,7 @@ return {
     vim.g.gitgutter_sign_added = "▍"
     vim.g.gitgutter_sign_removed = "◢"
     vim.g.gitgutter_sign_modified = "▍"
-    vim.g.gitgutter_sign_modified_removed = "▍"
+    vim.g.gitgutter_sign_modified_removed = "▍"
     vim.g.gitgutter_sign_removed_first_line = "◥"
     vim.g.gitgutter_eager = 1
     vim.g.gitgutter_realtime = 1
