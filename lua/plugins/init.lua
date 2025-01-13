e8f4b0681587f8b44283fdcc17b05c1e7f9be773
Michal Trybus <M.Trybus@adbglobal.com>
2025-01-13 10:40:59 +0100

    removed legacy plugin vim-markdown-toc (toc generation overtaken by marksman)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,7 +2,6 @@ return {
   'HiPhish/rainbow-delimiters.nvim',
   'bogado/file-line',
   'hynek/vim-python-pep8-indent',
-  'mzlogin/vim-markdown-toc',
   'rhysd/conflict-marker.vim',
   'ryvnf/readline.vim',
   'sotte/presenting.vim',
