65f9cdc06066db649d5f8c8221427106ebd26321
Michal Trybus <M.Trybus@adbglobal.com>
2025-01-16 12:58:26 +0100

    [gitgutter] changed removed lines sign

--- lua/plugins/gitgutter.lua
+++ lua/plugins/gitgutter.lua
@@ -6,7 +6,7 @@ return {
     vim.opt.signcolumn = 'yes'
     vim.g.gitgutter_override_sign_column_highlight = 0
     vim.g.gitgutter_sign_added = "▍"
-    vim.g.gitgutter_sign_removed = "◢"
+    vim.g.gitgutter_sign_removed = "▁"
     vim.g.gitgutter_sign_modified = "▍"
     vim.g.gitgutter_sign_modified_removed = "▍"
     vim.g.gitgutter_sign_removed_first_line = "◥"
