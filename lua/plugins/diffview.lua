29d3a93807e4115d548f3e0ac86e22cecb206c05
Michal Trybus <M.Trybus@adbglobal.com>
2025-01-31 07:49:49 +0100

    [diffview] hacked diffview to not wrap lines in diff mode

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -6,5 +6,13 @@ return {
     })
     -- workaround, because diffview sets this on setup based on DiffDelete instead of linking
     vim.cmd [[ hi DiffviewDiffAddAsDelete guibg=#200606 guifg=NONE guisp=#444444 gui=NONE ]]
+
+    -- Diffview hack???
+    vim.api.nvim_create_autocmd("OptionSet", {
+      pattern = "diff",
+      callback = function()
+        vim.opt_local.wrap = false
+      end,
+    })
   end
 }
