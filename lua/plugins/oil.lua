fe477b190b0bb04e45d3079ddc15c7fcd905b13d
Michal Trybus <M.Trybus@adbglobal.com>
2025-02-17 10:03:34 +0100

    [oil] added ORun for oil buffers to run a command with %O substituted current oil path

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,3 +1,18 @@
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "oil",
+  callback = function()
+    vim.api.nvim_buf_create_user_command(0, 'ORun', function(opts)
+      local dir = require('oil').get_current_dir(nil)
+      if dir == nil then
+        vim.notify("Oil not running", vim.log.levels.ERROR) -- should never happen...
+        return
+      end
+      local cmd = opts.args:gsub('%%O', dir)
+      vim.cmd(cmd)
+    end, { nargs = 1 })
+  end
+})
+
 return {
   'stevearc/oil.nvim',
 
