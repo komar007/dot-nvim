e5c61ade0f01d6a8c1008de05b51443ab7376cd0
Michal Trybus <komar007@gmail.com>
2025-03-29 12:34:11 +0100

    [flake] added a package definition for installation of nvim + dependencies as a package

--- flake.nix
+++ flake.nix
@@ -14,71 +14,77 @@
           unstable = import nixpkgs-unstable {
             inherit system;
           };
-        in
-        with stable; {
-          devShells.default = mkShell {
-            buildInputs = [
-              # Basic dependencies (lazy + fzf native compilation)
-              # ==================================================
-              git
-              cmake
-              # telescope
-              ripgrep
+          dependencies = with stable; [
+            # Basic dependencies (lazy + fzf native compilation)
+            # ==================================================
+            git
+            cmake
+            # telescope
+            ripgrep
 
-              # Neovim itself
-              # =============
-              neovim
+            # Neovim itself
+            # =============
+            neovim
 
-              # Language servers
-              # ================
+            # Language servers
+            # ================
 
-              # clangd
-              clang-tools
-              # dockerls
-              dockerfile-language-server-nodejs
-              # docker-compose-language-service
-              docker-compose-language-service
-              # gopls
-              go gopls
-              # marksman
-              (unstable.marksman.overrideAttrs (old: {
-                src = fetchFromGitHub {
-                  owner = "artempyanykh";
-                  repo = "marksman";
-                  # heading ID disambiguation fix (see: https://github.com/artempyanykh/marksman/issues/383)
-                  # nixpkgs-unstable is used as base, because tooling has been upgraded between 2024-10-07 and 2024-12-18
-                  rev = "2ae290a8c7352d349e1f7581fd757ce2d58268bf";
-                  hash = "sha256-eN3M2RaTUivtdLcRrpUuYoWmuPtlQycv6N/P9MwoRtM=";
-                };
-              }))
-              # jsonls
-              nodePackages.vscode-json-languageserver
-              # lua_ls
-              lua-language-server
-              # nixd
-              nixd nixpkgs-fmt
-              # eslint
-              nodePackages.eslint
-              # bashls
-              nodePackages_latest.bash-language-server
-              # pylsp
-              python312Packages.flake8 python312Packages.python-lsp-server
-              # vimls
-              vim-language-server
-              # yamlls
-              yaml-language-server
-              # protols
-              protols
+            # clangd
+            clang-tools
+            # dockerls
+            dockerfile-language-server-nodejs
+            # docker-compose-language-service
+            docker-compose-language-service
+            # gopls
+            go gopls
+            # marksman
+            (unstable.marksman.overrideAttrs (old: {
+              src = fetchFromGitHub {
+                owner = "artempyanykh";
+                repo = "marksman";
+                # heading ID disambiguation fix (see: https://github.com/artempyanykh/marksman/issues/383)
+                # nixpkgs-unstable is used as base, because tooling has been upgraded between 2024-10-07 and 2024-12-18
+                rev = "2ae290a8c7352d349e1f7581fd757ce2d58268bf";
+                hash = "sha256-eN3M2RaTUivtdLcRrpUuYoWmuPtlQycv6N/P9MwoRtM=";
+              };
+            }))
+            # jsonls
+            nodePackages.vscode-json-languageserver
+            # lua_ls
+            lua-language-server
+            # nixd
+            nixd nixpkgs-fmt
+            # eslint
+            nodePackages.eslint
+            # bashls
+            nodePackages_latest.bash-language-server
+            # pylsp
+            python312Packages.flake8 python312Packages.python-lsp-server
+            # vimls
+            vim-language-server
+            # yamlls
+            yaml-language-server
+            # protols
+            protols
 
-              # Other requirements
-              # ==================
+            # Other requirements
+            # ==================
 
-              # iamcco/markdown-preview.nvim plugin
-              yarn nodejs_22 nodePackages.npm
-              # for markdown formatting
-              pandoc
-            ];
+            # iamcco/markdown-preview.nvim plugin
+            yarn nodejs_22 nodePackages.npm
+            # for markdown formatting
+            pandoc
+          ];
+        in rec {
+          devShells.default = stable.mkShell {
+            buildInputs = dependencies;
           };
+          packages.nvim = stable.writeShellApplication {
+            name = "nvim";
+            runtimeInputs = dependencies;
+            text = ''nvim "$@"'';
+          };
+          packages.default = packages.nvim;
         }
       );
 }
