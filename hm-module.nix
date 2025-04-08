eb2b61adf47c15bfdf23de6ea6eb8595aed5f758
Michal Trybus <komar007@gmail.com>
2025-04-08 20:43:45 +0200

    [hm] selectively copying only relevant configuration files

--- hm-module.nix
+++ hm-module.nix
@@ -1,9 +1,35 @@
-{ config, nvim, ... }:
+{ config, lib, nvim, ... }:
+let
+  relPath = base: lib.strings.removePrefix (toString base + "/");
+  filteredPath =
+    base: patterns: builtins.path {
+      path = base;
+      filter =
+        nixPath: type:
+        let
+          path = relPath base nixPath + (if type == "directory" then "/" else "");
+        in builtins.foldl' (a: b: a||b) false (builtins.map
+          (
+            pat:
+              if lib.strings.hasSuffix "/" pat then
+                lib.strings.hasPrefix pat path
+              else
+                path == pat
+          )
+          patterns
+        );
+    };
+in
 {
   config.home.packages = [ nvim ];
 
   config.home.file.".config/nvim" = {
-    source = ./.;
-    recursive = true;
+    source = filteredPath ./. [
+      "init.lua"
+      "lazy-lock.json"
+      "legacy.vim"
+      "snippets/"
+      "lua/"
+    ];
   };
 }
