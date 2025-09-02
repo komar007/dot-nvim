fd494e92f2221b627af69a8199627f918b36e172
Michal Trybus <M.Trybus@adbglobal.com>
2025-09-02 17:54:25 +0200

    fix: added camelcasemotion mappings in visual mode

--- lua/plugins/camelcasemotion.lua
+++ lua/plugins/camelcasemotion.lua
@@ -1,11 +1,11 @@
 return {
   'bkad/CamelCaseMotion',
   keys = {
-    { "<leader>w",  "<Plug>CamelCaseMotion_w",  mode = { 'n', 'o' } },
+    { "<leader>w",  "<Plug>CamelCaseMotion_w",  mode = { 'n', 'o', 'x' } },
     { "i<leader>w", "<Plug>CamelCaseMotion_iw", mode = { 'o' } },
     { "<leader>iw", "<Plug>CamelCaseMotion_iw", mode = { 'o' } },
-    { "<leader>b",  "<Plug>CamelCaseMotion_b",  mode = { 'n', 'o' } },
-    { "<leader>e",  "<Plug>CamelCaseMotion_e",  mode = { 'n', 'o' } },
-    { "<leader>ge", "<Plug>CamelCaseMotion_ge", mode = { 'n', 'o' } },
+    { "<leader>b",  "<Plug>CamelCaseMotion_b",  mode = { 'n', 'o', 'x' } },
+    { "<leader>e",  "<Plug>CamelCaseMotion_e",  mode = { 'n', 'o', 'x' } },
+    { "<leader>ge", "<Plug>CamelCaseMotion_ge", mode = { 'n', 'o', 'x' } },
   },
 }
