c0fa2796ae91b59f7a7bbf84d459a37fc724be99
Michal Trybus <komar007@gmail.com>
2025-11-26 18:47:40 +0100

    style: reformatted all nix using nixfmt-rfc-style

--- flake.nix
+++ flake.nix
@@ -4,110 +4,118 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          stable = import nixpkgs {
-            inherit system;
-          };
-          unstable = import nixpkgs-unstable {
-            inherit system;
-          };
-          neovim = stable.neovim;
-          # provide the include path containing the well-known protos from protobuf,
-          # hijacking protols's pkg-config-based detection
-          protols_wrap_wellknown = pkg: stable.writeShellApplication {
+  outputs =
+    { flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        stable = import inputs.nixpkgs {
+          inherit system;
+        };
+        unstable = import inputs.nixpkgs-unstable {
+          inherit system;
+        };
+        neovim = stable.neovim;
+        # provide the include path containing the well-known protos from protobuf,
+        # hijacking protols's pkg-config-based detection
+        protols_wrap_wellknown =
+          pkg:
+          stable.writeShellApplication {
             name = "protols";
             text = ''
               ${pkg}/bin/protols -i "${stable.protobuf}/include" "$@"
             '';
           };
-          dependencies = with stable; [
-            # Basic dependencies (lazy + fzf native compilation)
-            # ==================================================
-            git
-            cmake
-            # telescope
-            ripgrep
+        dependencies = with stable; [
+          # Basic dependencies (lazy + fzf native compilation)
+          # ==================================================
+          git
+          cmake
+          # telescope
+          ripgrep
 
-            # Language servers
-            # ================
+          # Language servers
+          # ================
 
-            # clangd
-            clang-tools
-            # dockerls
-            dockerfile-language-server-nodejs
-            # docker-compose-language-service
-            docker-compose-language-service
-            # gopls
-            go gopls
-            # marksman
-            (unstable.marksman.overrideAttrs (old: {
-              src = fetchFromGitHub {
-                owner = "artempyanykh";
-                repo = "marksman";
-                # heading ID disambiguation fix (see: https://github.com/artempyanykh/marksman/issues/383)
-                # nixpkgs-unstable is used as base, because tooling has been upgraded between 2024-10-07 and 2024-12-18
-                rev = "2ae290a8c7352d349e1f7581fd757ce2d58268bf";
-                hash = "sha256-eN3M2RaTUivtdLcRrpUuYoWmuPtlQycv6N/P9MwoRtM=";
-              };
-            }))
-            # jsonls
-            nodePackages.vscode-json-languageserver
-            # jqls
-            unstable.jq-lsp
-            # taplo
-            taplo
-            # lua_ls
-            lua-language-server
-            # nixd
-            nixd nixfmt-rfc-style
-            # eslint
-            nodePackages.eslint
-            # bashls
-            unstable.bash-language-server
-            # pyright
-            pyright
-            # vacuum
-            vacuum-go
-            # vimls
-            vim-language-server
-            # yamlls
-            yaml-language-server
-            # protols
-            (protols_wrap_wellknown unstable.protols)
-            # ruff
-            ruff
-            # ts_ls
-            typescript-language-server
+          # clangd
+          clang-tools
+          # dockerls
+          dockerfile-language-server-nodejs
+          # docker-compose-language-service
+          docker-compose-language-service
+          # gopls
+          go
+          gopls
+          # marksman
+          (unstable.marksman.overrideAttrs (old: {
+            src = fetchFromGitHub {
+              owner = "artempyanykh";
+              repo = "marksman";
+              # heading ID disambiguation fix (see: https://github.com/artempyanykh/marksman/issues/383)
+              # nixpkgs-unstable is used as base, because tooling has been upgraded between 2024-10-07 and 2024-12-18
+              rev = "2ae290a8c7352d349e1f7581fd757ce2d58268bf";
+              hash = "sha256-eN3M2RaTUivtdLcRrpUuYoWmuPtlQycv6N/P9MwoRtM=";
+            };
+          }))
+          # jsonls
+          nodePackages.vscode-json-languageserver
+          # jqls
+          unstable.jq-lsp
+          # taplo
+          taplo
+          # lua_ls
+          lua-language-server
+          # nixd
+          nixd
+          nixfmt-rfc-style
+          # eslint
+          nodePackages.eslint
+          # bashls
+          unstable.bash-language-server
+          # pyright
+          pyright
+          # vacuum
+          vacuum-go
+          # vimls
+          vim-language-server
+          # yamlls
+          yaml-language-server
+          # protols
+          (protols_wrap_wellknown unstable.protols)
+          # ruff
+          ruff
+          # ts_ls
+          typescript-language-server
 
-            # Other requirements
-            # ==================
+          # Other requirements
+          # ==================
 
-            # iamcco/markdown-preview.nvim plugin
-            yarn nodejs_22 nodePackages.npm
-            # for markdown formatting
-            pandoc
-            # for shell formatting
-            shfmt
-          ];
-        in rec {
-          devShells.default = stable.mkShell {
-            buildInputs = dependencies ++ [ neovim ];
-          };
-          packages.nvim = stable.writeShellApplication {
-            name = "nvim";
-            runtimeInputs = dependencies;
-            text = ''
-              if [[ -v NVIM_XDG_CONFIG_HOME ]]; then
-                export XDG_CONFIG_HOME="''${NVIM_XDG_CONFIG_HOME}"
-              fi
-              ${neovim}/bin/nvim "$@"
-            '';
-          };
-          packages.default = packages.nvim;
-          homeManagerModules.default = args: import ./hm-module.nix (args // { nvim = packages.nvim; });
-        }
-      );
+          # iamcco/markdown-preview.nvim plugin
+          yarn
+          nodejs_22
+          nodePackages.npm
+          # for markdown formatting
+          pandoc
+          # for shell formatting
+          shfmt
+        ];
+      in
+      rec {
+        devShells.default = stable.mkShell {
+          buildInputs = dependencies ++ [ neovim ];
+        };
+        packages.nvim = stable.writeShellApplication {
+          name = "nvim";
+          runtimeInputs = dependencies;
+          text = ''
+            if [[ -v NVIM_XDG_CONFIG_HOME ]]; then
+              export XDG_CONFIG_HOME="''${NVIM_XDG_CONFIG_HOME}"
+            fi
+            ${neovim}/bin/nvim "$@"
+          '';
+        };
+        packages.default = packages.nvim;
+        homeManagerModules.default = args: import ./hm-module.nix (args // { nvim = packages.nvim; });
+      }
+    );
 }
--- hm-module.nix
+++ hm-module.nix
@@ -1,22 +1,19 @@
-{ config, lib, nvim, ... }:
+{ lib, nvim, ... }:
 let
   relPath = base: lib.strings.removePrefix (toString base + "/");
   filteredPath =
-    base: patterns: builtins.path {
+    base: patterns:
+    builtins.path {
       path = base;
       filter =
         nixPath: type:
         let
           path = relPath base nixPath + (if type == "directory" then "/" else "");
-        in builtins.foldl' (a: b: a||b) false (builtins.map
-          (
-            pat:
-              if lib.strings.hasSuffix "/" pat then
-                lib.strings.hasPrefix pat path
-              else
-                path == pat
-          )
-          patterns
+        in
+        builtins.foldl' (a: b: a || b) false (
+          builtins.map (
+            pat: if lib.strings.hasSuffix "/" pat then lib.strings.hasPrefix pat path else path == pat
+          ) patterns
         );
     };
 in
