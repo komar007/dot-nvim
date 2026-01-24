4c5c95fac37983771420b8feb92e7c084f7990a4
Michal Trybus <M.Trybus@adbglobal.com>
2026-01-24 13:21:13 +0100

    feat(rust): reload rust-analyzer on :Direnv reload if executable changes

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -1,3 +1,45 @@
+local function rust_analyzer_first_buffer()
+  for _, client in pairs(vim.lsp.get_clients()) do
+    if client.name == "rust-analyzer" then
+      for i, yes in pairs(client.attached_buffers) do
+        if yes then
+          return i
+        end
+      end
+    end
+  end
+  return nil
+end
+
+local function restart_rust_analyzer_if_changed()
+  for _, client in pairs(vim.lsp.get_clients()) do
+    if client.name == "rust-analyzer" then
+      local current_ra_exe = client.config.cmd[1]
+      local result = vim.system({ "which", "rust-analyzer" }, { text = true }):wait()
+      local system_ra_exe = vim.trim(result.stdout)
+      if current_ra_exe ~= system_ra_exe then
+        -- :RustAnalyzer wants to be run in the context of a buffer rust-analyzer is attached to...
+        local ra_buf = rust_analyzer_first_buffer()
+        if ra_buf == nil then
+          vim.notify(
+            "could not restart rust-analyzer: no attached buffer\nold=" .. current_ra_exe .. "\nnew=" .. system_ra_exe,
+            vim.log.levels.WARN
+          )
+          return
+        end
+        vim.notify(
+          "restarting rust-analyzer\nold=" .. current_ra_exe .. "\nnew=" .. system_ra_exe,
+          vim.log.levels.INFO
+        )
+        vim.api.nvim_buf_call(ra_buf, function()
+          vim.cmd.RustAnalyzer("restart")
+        end)
+        return
+      end
+    end
+  end
+end
+
 return {
   'mrcjkb/rustaceanvim',
   version = '^7',
@@ -43,5 +85,12 @@ return {
         },
       },
     }
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "DirenvLoaded",
+      callback = function()
+        restart_rust_analyzer_if_changed()
+      end,
+    })
   end,
 }
