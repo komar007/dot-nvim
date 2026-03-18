c4b7325ab8c399976047b9dd5848875e54d8712c
Michal Trybus <M.Trybus@adbglobal.com>
2026-03-18 15:10:08 +0100

    fix(wrapper): unset XDG_CONFIG_HOME so that it is not propagated to spawned processes

--- nix_run_nvim_with_here_config.sh
+++ nix_run_nvim_with_here_config.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 
+# Run neovim with all provided arguments and config directory set to this directory instead of the
+# default.
+#
+# This is done by setting XDG_CONFIG_HOME to this directory. Neovim reads the config from
+# $XDG_CONFIG_HOME/nvim, so we also have a symbolic link ./nvim -> ./.
+#
+# Unwanted propagation of XDG_CONFIG_HOME to processes spawned by neovim (like git, for example) is
+# mitigated by immediately removing this variable inside neovim's session by injecting an unlet
+# command
+
 DIR=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)
 
-if nix --version > /dev/null; then
-	exec nix run -s XDG_CONFIG_HOME "$DIR" "$DIR" -- "$@"
+if nix --version >/dev/null; then
+	# shellcheck disable=SC2016
+	exec nix run -s XDG_CONFIG_HOME "$DIR" "$DIR" -- \
+		-c "unlet \$XDG_CONFIG_HOME" \
+		"$@"
 else
-	exec env "XDG_CONFIG_HOME=$DIR" nvim "$@"
+	exec env "XDG_CONFIG_HOME=$DIR" nvim \
+		-c "unlet \$XDG_CONFIG_HOME" \
+		"$@"
 fi
