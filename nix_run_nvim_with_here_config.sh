c9bd854d383647fabfb9ce322e3a224dde816af8
Michal Trybus <M.Trybus@adbglobal.com>
2026-04-20 19:32:52 +0200

    feat(wrapper): add option to use config in repo directly
    It used to be the default, now both regular and alternative modes are
    run with a generated config (~/.config/nvim generated by HM or
    a temporary config build, respectively). The ability to run with the
    config pointing to the repository itself is required to allow lazy to
    modify its lazy-lock.json file and it seems to be the sole case.

--- nix_run_nvim_with_here_config.sh
+++ nix_run_nvim_with_here_config.sh
@@ -9,9 +9,23 @@
 # In nix mode, a dummy home-manager configuration is built (containing just wrapped neovim and
 # config) and XDG_CONFIG_HOME is set to the built configuration.
 #
+# Running with XDG_CONFIG_HOME set to the built configuration has the advantage of vim being run on
+# mostly(*) read-only configuration files so it cannot modify anything, especially lazy-lock.json.
+# This is also a disadvantage for the very same reason - it is not possible to use :Lazy inside
+# neovim started by this script to update lazy-lock.nvim (plugins themselves are managed in
+# ~/.local/share/nvim which we do not override here).
+#
+# That's why this script accepts NIX_RUN_NVIM_WITH_HERE_CONFIG_GENERATED=false in which case,
+# instead of setting XDG_CONFIG_HOME to the generated config, it sets it to this repository. This
+# can be used to perform lazy-nvim tasks in a non-sandboxed environment.
+#
 # Unwanted propagation of XDG_CONFIG_HOME to processes spawned by neovim (like git, for example) is
 # mitigated by immediately removing this variable inside neovim's session by injecting an unlet
 # command
+#
+# (*) - currently we are using recursive = true for config.home.file.".config/nvim", but it is just
+# because it was easier to add generation of quirks.lua this way. This should probably be changed
+# one day.
 
 set -e
 
@@ -26,7 +40,12 @@ if nix --version >/dev/null; then
 		--out-link "$RESULT" \
 		--impure \
 		"${DIR}#homeConfigurations.${SYSTEM}.default.activationPackage"
-	exec env XDG_CONFIG_HOME="$RESULT/home-files/.config/" "$RESULT/home-path/bin/nvim" \
+	if [ "${NIX_RUN_NVIM_WITH_HERE_CONFIG_GENERATED}" = false ]; then
+		CONFIG_DIR="$DIR"
+	else
+		CONFIG_DIR="$RESULT/home-files/.config/"
+	fi
+	exec env XDG_CONFIG_HOME="$CONFIG_DIR" "$RESULT/home-path/bin/nvim" \
 		-c "$EXTRA_CMD" \
 		"$@"
 else
