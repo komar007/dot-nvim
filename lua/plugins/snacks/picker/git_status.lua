a78d491423e36b62b2260fd8cfb7e0ee1a33621b
Michal Trybus <M.Trybus@adbglobal.com>
2026-05-13 15:57:25 +0200

    fix(picker): eager use of snacks in snacks' config would fail on first neovim start

--- lua/plugins/snacks/picker/git_status.lua
+++ lua/plugins/snacks/picker/git_status.lua
@@ -1,11 +1,10 @@
-local git = require('snacks.picker.source.git')
-
 ---@type snacks.picker.Config
 return {
   matcher = {
     sort_empty = true,
   },
   sort = function(a, b)
+    local git = require('snacks.picker.source.git')
     local a_unmerged = git.git_status(a.status).unmerged or false
     local b_unmerged = git.git_status(b.status).unmerged or false
     if a_unmerged ~= b_unmerged then
