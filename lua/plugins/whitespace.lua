fe5ec98437f75bdf4cdd7c46e5f3332bfe8b4bd8
Michal Trybus <komar007@gmail.com>
2026-05-15 20:02:42 +0200

    refactor(whitespace): remove now-default ignore-list & upgrade plugin

--- lua/plugins/whitespace.lua
+++ lua/plugins/whitespace.lua
@@ -4,7 +4,6 @@ return {
     vim.cmd [[ hi ExtraWhitespace guibg=#ff7aa8 ]]
     return {
       highlight = "ExtraWhitespace",
-      ignored_filetypes = { 'TelescopePrompt', 'Trouble', 'help', 'dashboard', "snacks_picker_input" },
     }
   end,
 }
