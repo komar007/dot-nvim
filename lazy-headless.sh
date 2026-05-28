373f007d03da43cdf25c0128255bff884a1a316b
Michal Trybus <M.Trybus@adbglobal.com>
2026-05-28 23:30:58 +0200

    feat(wrapper): make lazy-headless.sh always generate data from scratch
    This makes sure it does not mess with the here-config's data and has
    observable side-effects exclusively in lazy-lock.json.

--- lazy-headless.sh
+++ lazy-headless.sh
@@ -2,6 +2,13 @@
 
 DIR=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)
 
+T=$(mktemp -d)
+cleanup() {
+	rm -fr "$T"
+}
+trap cleanup EXIT
+
+export HERE_CONFIG_BASE="$D"
 export LAZY_NVIM_LOCKFILE="$DIR"/lazy-lock.json
 export LAZY_NVIM_UNLOCKED=true
 "$DIR"/nix_run_nvim_with_here_config.sh --headless "+LazyHeadless $*"
