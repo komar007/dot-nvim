4d4a85ce23e90fa21d692c74a77e96230b1731e5
Michal Trybus <M.Trybus@adbglobal.com>
2026-05-28 15:35:19 +0200

    feat(hm): add configurable config directory name

--- hm-module.nix
+++ hm-module.nix
@@ -22,23 +22,30 @@ let
         );
     };
   branchSymbol = config.dot-nvim.quirks.gitBranchSymbol;
+  appname = config.dot-nvim.appname;
 in
 {
-  options.dot-nvim.quirks.gitBranchSymbol = lib.mkOption {
-    type = lib.types.str;
-    description = "the symbol used to represent a git branch";
-    default = "⎇";
-  };
-
-  options.dot-nvim.lazy.locked = lib.mkOption {
-    type = lib.types.bool;
-    description = "verify that lazy-lock.json is in sync with plugin config during activation";
-    default = true;
+  options.dot-nvim = {
+    appname = lib.mkOption {
+      type = lib.types.str;
+      description = "the name of the config directory in ~/.config/ to populate";
+      default = "nvim";
+    };
+    quirks.gitBranchSymbol = lib.mkOption {
+      type = lib.types.str;
+      description = "the symbol used to represent a git branch";
+      default = "⎇";
+    };
+    lazy.locked = lib.mkOption {
+      type = lib.types.bool;
+      description = "verify that lazy-lock.json is in sync with plugin config during activation";
+      default = true;
+    };
   };
 
   config.home.packages = [ nvim ];
 
-  config.home.file.".config/nvim" = {
+  config.home.file.".config/${appname}" = {
     source = filteredPath ./. [
       "init.lua"
       "lazy-lock.json"
@@ -76,7 +83,7 @@ in
     '';
   };
 
-  config.home.file.".config/nvim/lua/quirks.lua".text = ''
+  config.home.file.".config/${appname}/lua/quirks.lua".text = ''
     return {
       git_branch_symbol = '${branchSymbol}',
     }
