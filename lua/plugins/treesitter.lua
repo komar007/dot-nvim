3f6f154c54b65de0ade36d896737f85dbc7de7db
Michal Trybus <M.Trybus@adbglobal.com>
2026-07-20 12:04:04 +0200

    fix(treesitter): start wherever possible

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -31,5 +31,21 @@ return {
       "xml",
       "yaml",
     }):wait(300000)
+
+    local group = vim.api.nvim_create_augroup("TreesitterHighlight", { clear = true })
+    vim.api.nvim_create_autocmd("FileType", {
+      group = group,
+      callback = function(args)
+        local lang = vim.treesitter.language.get_lang(args.match)
+        if not lang then
+          return
+        end
+        local parser_available = vim.treesitter.language.add(lang)
+        if not parser_available then
+          return
+        end
+        vim.treesitter.start(args.buf, lang)
+      end,
+    })
   end,
 }
