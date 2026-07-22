17a645b9a6c470ed15d82d3b321f4711bfa701b3
Michal Trybus <M.Trybus@adbglobal.com>
2026-07-22 10:10:07 +0200

    feat: add winsep plugin

---
+++ lua/plugins/winsep.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-zh/colorful-winsep.nvim",
+  event = { "WinLeave" },
+  opts = {
+    colors = { "#333352" },
+    border = "single",
+    animate = {
+      enabled = false,
+    },
+  },
+}
