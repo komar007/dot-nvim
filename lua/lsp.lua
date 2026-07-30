b7d828d84f65e19016c32f8d9abd263c3bb46d06
Michal Trybus <M.Trybus@adbglobal.com>
2026-07-30 08:50:43 +0200

    feat(lua): replace lazydev with emmylua_ls config recommended by nvim-lspconfig

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -89,7 +89,31 @@ require('utils').setup_lsps(
       capabilities = caps_for_jsonls,
       cmd = { "vscode-json-languageserver", "--stdio" },
     },
-    'emmylua_ls',
+    {
+      'emmylua_ls',
+      -- the following config is based on
+      -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#emmylua_ls
+      on_init = function(client)
+        if client.workspace_folders then
+          local path = client.workspace_folders[1].name
+          if
+            path ~= vim.fn.stdpath('config')
+            and (vim.uv.fs_stat(path .. '/.emmyrc.json') or vim.uv.fs_stat(path .. '/.luarc.json'))
+          then
+            client.config.settings = {}
+          end
+        end
+      end,
+      settings = {
+        emmylua = {
+          runtime = { version = 'LuaJIT' },
+          diagnostics = { globals = { 'vim' } },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file('', true),
+          },
+        },
+      },
+    },
     'marksman',
     {
       'nixd',
