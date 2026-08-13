fe56d0a43faacb0e1d9d38296f0f6acf198a0339
Michal Trybus <M.Trybus@adbglobal.com>
2026-08-13 13:43:25 +0200

    feat(typos): disable diagnostics in readonly buffers (includes man for example)

--- lua/plugins/typos.lua
+++ lua/plugins/typos.lua
@@ -1,4 +1,17 @@
 return {
   'poljar/typos.nvim',
-  opts = {},
+  config = function()
+    require('typos').setup()
+
+    local typos_ns = vim.api.nvim_create_namespace('typos')
+    local group = vim.api.nvim_create_augroup('typos-readonly-diagnostics', { clear = true })
+    vim.api.nvim_create_autocmd('DiagnosticChanged', {
+      group = group,
+      callback = function(args)
+        if vim.bo[args.buf].readonly and #vim.diagnostic.get(args.buf, { namespace = typos_ns }) > 0 then
+          vim.diagnostic.reset(typos_ns, args.buf)
+        end
+      end,
+    })
+  end,
 }
