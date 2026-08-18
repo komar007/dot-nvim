1f290fd423bf1db50ce251d0196cf13bb858cc68
Michal Trybus <M.Trybus@adbglobal.com>
2026-08-18 14:06:20 +0200

    fix(title): show filename correctly in Man! mode, make filename optional

--- lua/title.lua
+++ lua/title.lua
@@ -1,9 +1,22 @@
 local utils = require("utils")
 
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-  pattern = "*",
-  callback = function()
-    vim.o.titlestring = "nvim:  " .. vim.fn.expand("%:t") .. " in  " .. utils.shorten_path(vim.uv.cwd())
+visible_bufnr = 0
+vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
+  callback = function(ev)
+    if ev.event == "BufEnter" then
+      visible_bufnr = ev.buf
+    end
+    if ev.event == "FileType" and ev.buf ~= visible_bufnr then
+      return
+    end
+    local path = vim.api.nvim_buf_get_name(ev.buf)
+    local title = "nvim"
+    if path ~= "" then
+      local filename = vim.fs.basename(path)
+      title = title .. ":  " .. filename
+    end
+    title = title .. " in  " .. utils.shorten_path(vim.uv.cwd())
+    vim.o.titlestring = title
   end
 })
 vim.opt.title = true
