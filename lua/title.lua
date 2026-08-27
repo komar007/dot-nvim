10dab943f7948cb987eed05f4a251a2a88ca79be
Michal Trybus <M.Trybus@adbglobal.com>
2026-08-27 12:24:56 +0200

    feat(title): support oil:// buffers

--- lua/title.lua
+++ lua/title.lua
@@ -7,6 +7,21 @@ local ll_filetype = require('lualine.components.filetype'):new({
   padding = 0
 })
 
+local function strip_prefix(str, prefix)
+  if str:sub(1, #prefix) == prefix then
+    return str:sub(#prefix + 1)
+  end
+  return nil
+end
+
+local function basename(filename)
+  if filename:sub(-1) == "/" then
+    return vim.fs.basename(filename:sub(1, -2)) .. "/"
+  else
+    return vim.fs.basename(filename)
+  end
+end
+
 visible_bufnr = 0
 vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
   callback = function(ev)
@@ -18,11 +33,24 @@ vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
     end
     local path = vim.api.nvim_buf_get_name(ev.buf)
     local title = "nvim"
+    local is_oil = false
     if path ~= "" then
-      local filename = vim.fs.basename(path)
-      local icon = ll_filetype:draw("", true)
-      if icon == "" then
-        icon = ""
+      local filename = strip_prefix(path, "oil://")
+      if filename then
+        is_oil = true
+      else
+        filename = path
+      end
+
+      filename = basename(filename)
+      local icon
+      if is_oil then
+        icon = "󰙅"
+      else
+        icon = ll_filetype:draw("", true)
+        if icon == "" then
+          icon = ""
+        end
       end
       title = title .. ": " .. icon .. " " .. filename
     end
